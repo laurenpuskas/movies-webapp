9d788b174144884f6ce15c1730de3e4c7caf0183
Liliripe <liliripe.studio@gmail.com>
2021-10-27 14:42:21 -0500

    init api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const apiUrl = 'https://wookie.codesubmit.io/movies';
+
+const requestOptions = {
+  headers: { Authorization: 'Bearer Wookie2021' },
+};
 
 function App() {
+  // set state for movies display
+  const [movies, setMovies] = useState([]);
+
+  // fetch api data
+  useEffect(() => {
+    async function fetchData() {
+      await fetch(apiUrl, requestOptions)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setMovies(data.movies);
+        })
+        .catch((error) => {
+          console.log('error: ' + error);
+        });
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* backdrop, cast, classification, director, genres, id, imdb_rating,
+      length, overview, poster, released_on, slug, title */}
+      {movies.map((movie, index) => (
+        <div key={index}>{movie.title}</div>
+      ))}
     </div>
   );
 }
